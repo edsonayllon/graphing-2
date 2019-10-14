0e08f561465fb621ab3e3f68c2da1721da83dfb9
Edson Ayllon <eayllon1@gmail.com>
2019-10-14 18:29:43 -0400

    add charts

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,56 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Doughnut, Line } from 'react-chartjs-2';
+
+const data = {
+	labels: [
+		'Red',
+		'Green',
+		'Yellow'
+	],
+	datasets: [{
+		data: [300, 50, 100],
+		backgroundColor: [
+		'#FF6384',
+		'#36A2EB',
+		'#FFCE56'
+		],
+		hoverBackgroundColor: [
+		'#FF6384',
+		'#36A2EB',
+		'#FFCE56'
+		]
+	}]
+};
+
+const data2 = {
+  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+  datasets: [
+    {
+      label: 'My First dataset',
+      fill: false,
+      lineTension: 0.1,
+      backgroundColor: 'rgba(75,192,192,0.4)',
+      borderColor: 'rgba(75,192,192,1)',
+      borderCapStyle: 'butt',
+      borderDash: [],
+      borderDashOffset: 0.0,
+      borderJoinStyle: 'miter',
+      pointBorderColor: 'rgba(75,192,192,1)',
+      pointBackgroundColor: '#fff',
+      pointBorderWidth: 1,
+      pointHoverRadius: 5,
+      pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+      pointHoverBorderColor: 'rgba(220,220,220,1)',
+      pointHoverBorderWidth: 2,
+      pointRadius: 1,
+      pointHitRadius: 10,
+      data: [65, 59, 80, 81, 56, 55, 40]
+    }
+  ]
+};
+
 
 function App() {
   return (
@@ -19,6 +69,14 @@ function App() {
           Learn React
         </a>
       </header>
+      <div>
+        <h2>Doughnut Example</h2>
+        <Doughnut data={data} />
+      </div>
+      <div>
+        <h2>Line Example</h2>
+        <Line data={data2} />
+      </div>
     </div>
   );
 }
